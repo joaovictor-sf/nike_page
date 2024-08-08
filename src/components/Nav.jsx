9473a1dba5f318a811f65c2076b21dfa82e3d1f6
João Victor Santana Ferreira <joaovictorsantanaf@gmail.com>
2024-08-08 19:29:17 -0300

    add Nav

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,8 +1,26 @@
-import React from 'react'
+import {headerLogo} from '../assets/images';
+import { hamburger } from '../assets/icons';
+import { navLinks } from '../constants';
 
 const Nav = () => {
   return (
-    <div>Nav</div>
+    <header className='padding-x py-8 absolute z-10 w-full'>
+        <nav className='flex justify-between items-center max-container'>
+            <a href="/">
+                <img src={headerLogo} alt="Logo" height={29} width={130}/>
+            </a>
+            <ul className='flex flex-1 justify-center items-center gap-16 max-lg:hidden'>
+                {navLinks.map((item) => (
+                    <li key={item.label}>
+                        <a href={item.href} className='font-monts errat leading-normal text-lg text-slate-gray'>{item.label}</a>
+                    </li>
+                ))}
+            </ul>
+            <div className='lg:hidden cursor-pointer'>
+                <img src={hamburger} alt="hamburger" width={25} height={25}/>
+            </div>
+        </nav>
+    </header>
   )
 }
 
